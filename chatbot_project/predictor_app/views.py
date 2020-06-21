ddadc6e7293ba04e29bc045483590b479a9a7c72
mashuqur rahman <mashuq.tanmoy06@gmail.com>
2020-06-21 20:34:27 +0600

    json response commit

--- chatbot_project/predictor_app/views.py
+++ chatbot_project/predictor_app/views.py
@@ -10,6 +10,7 @@ from predictor_app.serializers import ChatSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
 
 # Create your views here.
 @csrf_exempt
@@ -35,8 +36,10 @@ def BotView(request):
                'designation':designation_id,
 
             }
+            # final_data=JSONRenderer().render(final_data)
+            final_data=json.dumps(final_data)
       
-            return Response(final_data)
+            return JsonResponse(final_data,safe=False)
 
 @csrf_exempt
 @api_view(['GET','POST'])
@@ -55,7 +58,8 @@ def FullresponseView(request):
             Chat.objects.create( officer_id =officer_id, office_id = office_id,designation_id=designation_id,question=question,answer=answer[0]['answer'])
             final_data=Chat.objects.filter(officer_id=officer_id).filter(office_id=office_id)
             serializer=ChatSerializer(final_data,many=True)
-            return Response(serializer.data)
+            
+            return JsonResponse(serializer.data, safe=False)
            
 def HomeView(request):
   return render(request,'predictor_app/home.html') 
